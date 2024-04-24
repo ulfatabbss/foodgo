f7ea8b67930ede5f4ed978dd0c643806ef2c5a6e
Malaika78 <malaika786jutt@gmail.com>
2024-04-24 17:55:52 +0500

    changes

--- src/routes/AppStack.js
+++ src/routes/AppStack.js
@@ -1,12 +1,20 @@
 import {StyleSheet, Text, View} from 'react-native';
 
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from '../Screens/Home';
+import Splash from '../Screens/Splash';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 const AppStack = () => {
   return (
-    <View>
-      <Text>AppStack</Text>
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Splash" component={Splash} />
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
